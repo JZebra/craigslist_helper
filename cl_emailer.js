07d72cd2bf0051fec0edd171d00dd5e5dde593a6
John Shen <johnzhihaoshen@gmail.com>
2018-02-06 14:28:59 -0800

    getEmailLink works

--- cl_emailer.js
+++ cl_emailer.js
@@ -1,7 +1,23 @@
+const LISTING_CLASS = 'result-info';
+const ANCHOR_CLASS = 'result-title';
+// const urlRegionMap is defined in cl_codes.js
+
+const getEmailLink = (listing) => {
+  const listUrlArr = listing.getElementsByClassName(ANCHOR_CLASS)[0]
+    .getAttribute('href')
+    .replace(/^https?:\/\//i, '')
+    .split('/');
+  const host = listUrlArr[0];
+  const regionCode = urlRegionMap[host];
+  const listingCode = listUrlArr[listUrlArr.length - 1].replace('.html', '');
+  const category = listUrlArr[2];
+  return `http://${host}/reply/${regionCode}/${category}/${listingCode}`;
+};
+
 const addEmailLinks = () => {
-  let listings = document.getElementsByClassName('result-info');
+  let listings = document.getElementsByClassName(LISTING_CLASS);
   for (let i = 0; i < listings.length; i++) {
-    console.log('appending')
+    console.log(getEmailLink(listings[i]));
     const emailButton = document.createTextNode("Email");
     listings[i].append(emailButton);
   }
