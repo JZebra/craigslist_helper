353f447d3d79fd5a3cda5ff4ce71aff7b3483ac4
John Shen <johnzhihaoshen@gmail.com>
2018-02-07 23:35:18 -0800

    getReplyEmail can parse email address, subject, and body from response html

--- cl_emailer.js
+++ cl_emailer.js
@@ -14,15 +14,44 @@ const getEmailUrl = (listing) => {
   return `${scheme}//${host}/reply/${regionCode}/${category}/${listingCode}`;
 };
 
+const getReplyEmail = (resp) => {
+  // eg mailto:q7v4b-6463758095@sale.craigslist.org?subject=new+and+used+exercise+bikes&body=%0A%0Ahttps%3A%2F%2Fsfbay.craigslist.org%2Feby%2Fsgd%2Fd%2Fnew-and-used-exercise-bikes%2F6463758095.html%0A
+  const [email, queryStr] = resp.getElementsByClassName('mailapp')[0].href
+    .replace('mailto:', '')
+    .split('?');
+  const [subject, body] = decodeURIComponent(queryStr)
+    .split('&')
+    .map(el => el.split('=')[1]);
+
+  console.log(email, subject, body);
+};
+
 const addEmailLinks = () => {
   const listings = document.getElementsByClassName(LISTING_CLASS);
   for (let i = 0; i < listings.length; i++) {
     const emailButton = document.createElement('span');
     emailButton.appendChild(document.createTextNode('Email'));
-    const req = new XMLHttpRequest();
-    req.open('GET', getEmailUrl(listings[i]));
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', getEmailUrl(listings[i]), true);
+    // necessary for html parsing
+    xhr.responseType = 'document';
+
+    xhr.onload = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          getReplyEmail(xhr.responseXML);
+        } else {
+          console.error(xhr.statusText);
+        }
+      }
+    };
+
+    xhr.onerror = () => {
+      console.error(xhr.statusText);
+    };
+
     emailButton.addEventListener('click', () => {
-      req.send();
+      xhr.send();
     });
     listings[i].append(emailButton);
   }
